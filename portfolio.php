f2fc450424dd9605b96d42985b5b8b7c8960e1e4
drshnkini <alphacoresolutions@gmail.com>
2023-02-21 21:44:59 +0530

    menu updated

--- portfolio.php
+++ portfolio.php
@@ -53,128 +53,145 @@ include('header.php');
         ?>
 
 
-
         <div class="portfolio">
             <ul>
                 <?php $category_sql = $hobj->select("wm_catid,wm_name", "wm_cat", "wm_status='Enable'");
-                            if ($category_sql != 'no') {
-                                foreach ($category_sql as $category) { ?>
-                <li class="arrow"><a href="#" class="submenu"><?= $category['wm_name'] ?></a>
-                    <ul class="sub-menu">
-                        <?php $check_subcat_sql = $hobj->select("wm_subcatid,wm_subname", "wm_subcat", "wm_substatus='Enable' AND wm_catid_sub='" . $category['wm_catid'] . "'");
-                                            if ($check_subcat_sql != 'no') {
-                                                foreach ($check_subcat_sql as $subcat) { ?>
-                        <li class="arrow"><a href="#" class="submenu"><?= $subcat['wm_subname']; ?></a>
-                            <ul class="sub-menu sub-menu2 scroll">
-                                 <?php $check_year_sql = $hobj->select("wm_yearid,wm_year", "wm_year", "wm_yearstatus='Enable' AND wm_subcatid_year='" . $subcat['wm_subcatid'] . "'");
-                                                            if ($check_year_sql != 'no') {
-                                                                foreach ($check_year_sql as $year) { ?>
-                                <li><a href="#" class="submenu"><?= $year['wm_year']; ?></a>
-                                    <ul>
-                                                                        <?php $check_product_sql = $hobj->select("*", "wm_product", "wm_prostatus='Enable' AND wm_catid_product='" . $category['wm_catid'] . "' AND wm_subcatid_product='" . $subcat['wm_subcatid'] . "' AND wm_year_product='" . $year['wm_yearid'] . "' group by wm_product");
-                                                                        if ($check_product_sql != 'no') { ?>
-                                                                            <?php foreach ($check_product_sql as $product) {
-                                                                                $categoryId = $category['wm_catid'];
-                                                                                $subcategory_id = $subcat['wm_subcatid'];
-                                                                                $year = $product['wm_year_product'];
-                                                                                $productname = $product['wm_product'];
-                                                                                $proid = $product['wm_proid']; ?>
-                                                                                <li>
-                                                                                    <a href='portfolio.php?id=<?= $proid ?>&cat=<?= $categoryId ?>&subcat=<?= $subcategory_id ?>&year=<?= $year ?>&name=<?= $productname ?>'><?= $product['wm_product'] ?></a>
-                                                                                </li>
-                                                                            <?php } } ?>
-                                                                        </ul>
-                                </li>
-                                 <?php } } ?>
+                if ($category_sql != 'no') {
+                    foreach ($category_sql as $category) { ?>
+                        <li class="arrow"><a href="#" class="submenu"><?= $category['wm_name'] ?></a>
+                            <ul class="sub-menu">
+                                <?php $check_subcat_sql = $hobj->select("wm_subcatid,wm_subname", "wm_subcat", "wm_substatus='Enable' AND wm_catid_sub='" . $category['wm_catid'] . "'");
+                                if ($check_subcat_sql != 'no') {
+                                    foreach ($check_subcat_sql as $subcat) { ?>
+                                        <li class="arrow"><a href="#" class="submenu"><?= $subcat['wm_subname']; ?></a>
+                                            <ul class="sub-menu sub-menu2 scroll">
+                                                <?php $check_year_sql = $hobj->select("wm_yearid,wm_year", "wm_year", "wm_yearstatus='Enable' AND wm_subcatid_year='" . $subcat['wm_subcatid'] . "'");
+                                                if ($check_year_sql != 'no') {
+                                                    foreach ($check_year_sql as $year) { ?>
+                                                        <li><a href="#" class="submenu"><?= $year['wm_year']; ?></a>
+                                                            <ul>
+                                                                <?php $check_product_sql = $hobj->select("*", "wm_product", "wm_prostatus='Enable' AND wm_catid_product='" . $category['wm_catid'] . "' AND wm_subcatid_product='" . $subcat['wm_subcatid'] . "' AND wm_year_product='" . $year['wm_yearid'] . "' group by wm_product");
+                                                                if ($check_product_sql != 'no') { ?>
+                                                                    <?php foreach ($check_product_sql as $product) {
+                                                                        $categoryId = $category['wm_catid'];
+                                                                        $subcategory_id = $subcat['wm_subcatid'];
+                                                                        $year = $product['wm_year_product'];
+                                                                        $productname = $product['wm_product'];
+                                                                        $proid = $product['wm_proid']; ?>
+                                                                        <li>
+                                                                            <a href='portfolio.php?id=<?= $proid ?>&cat=<?= $categoryId ?>&subcat=<?= $subcategory_id ?>&year=<?= $year ?>&name=<?= $productname ?>'><?= $product['wm_product'] ?></a>
+                                                                        </li>
+                                                                    <?php }
+                                                                } ?>
+                                                            </ul>
+                                                        </li>
+                                                    <?php }
+                                                } ?>
+                                            </ul>
+                                        </li>
+                                    <?php }
+                                }else {
+                                    $check_product_sql = $hobj->select("*", "wm_product", "wm_prostatus='Enable' AND wm_catid_product='" . $category['wm_catid'] . "' group by wm_product");
+                                    if ($check_product_sql != 'no') {
+                                        foreach ($check_product_sql as $product) {
+                                            $categoryId = $category['wm_catid'];
+                                            $year = $product['wm_year_product'];
+                                            $productname = $product['wm_product'];
+                                            $proid = $product['wm_proid']; ?>
+
+                                            <li>
+                                                <a href='portfolio.php?id=<?= $proid ?>&cat=<?= $categoryId ?>&subcat=<?= $subcategory_id ?>&year=<?= $year ?>&name=<?= $productname ?>'><?= $product['wm_product'] ?></a>
+                                            </li>
+                                        <?php }
+                                    }
+                                } ?> 
                             </ul>
                         </li>
-                         <?php } } ?>
-                    </ul>
-                </li>
-                 <?php } } ?>
+                    <?php }
+                } ?>
             </ul>
             </ul>
         </div>
     </div>
     <div class="rightDiv">
-            <div class="span6 offset3">
-                <div id="myCarousel" class="carousel slide">
-                    <ol class="carousel-indicators">
-                        <li data-target="#myCarousel" data-slide-to="0" class=""></li>
-                        <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
-                        <li data-target="#myCarousel" data-slide-to="2" class=""></li>
-                    </ol>
-                    <div class="carousel-inner">
+        <div class="span6 offset3">
+            <div id="myCarousel" class="carousel slide">
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+                    <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+                </ol>
+                <div class="carousel-inner">
 
 
-                        <?php
-                        if (isset($_REQUEST['cat']) && isset($_REQUEST['subcat']) && isset($_REQUEST['year']) && isset($_REQUEST['name'])) {
-                            $cat = $_REQUEST['cat'];
-                            if ($_REQUEST['subcat'] != '0' && $_REQUEST['year'] != '0') {
-                                $sub = $_REQUEST['subcat'];
-                                $year_id = $_REQUEST['year'];
-                                $pro_name = $_REQUEST['name'];
-                                //print_r($pro_name);
-                                $ans = $hobj->select("*", "wm_product", "wm_catid_product='$cat' AND wm_subcatid_product='$sub' AND wm_year_product='$year_id' AND wm_product='$pro_name'");
-                                $i = 0;
-                                $len = count($ans);
-                                foreach ($ans as $imagepath) {
-                                    $imagepath = $imagepath['wm_path'];
-                                    //$siteurl="http://water-mark.in/control_panel";
-                                    //print_r($key);
-                                    if ($i == 0) {
-                                        // first
-                                        echo '<div class="item active">';
-                                        echo '<img src="' . $siteurl . "/" . $imagepath . '" alt="">';
-                                        echo '</div>';
-                                    }
-                                    if ($i != 0) {
-                                        echo "<div class='item'>";
-                                        echo '<img src="' . $siteurl . "/" . $imagepath . '" alt="">';
-                                        echo "</div>";
-                                    }
-                                    $i++;
-                                    //print_r($siteurl."/".$imagepath);
+                    <?php
+                    if (isset($_REQUEST['cat']) && isset($_REQUEST['subcat']) && isset($_REQUEST['year']) && isset($_REQUEST['name'])) {
+                        $cat = $_REQUEST['cat'];
+                        if ($_REQUEST['subcat'] != '0' && $_REQUEST['year'] != '0') {
+                            $sub = $_REQUEST['subcat'];
+                            $year_id = $_REQUEST['year'];
+                            $pro_name = $_REQUEST['name'];
+                            //print_r($pro_name);
+                            $ans = $hobj->select("*", "wm_product", "wm_catid_product='$cat' AND wm_subcatid_product='$sub' AND wm_year_product='$year_id' AND wm_product='$pro_name'");
+                            $i = 0;
+                            $len = count($ans);
+                            foreach ($ans as $imagepath) {
+                                $imagepath = $imagepath['wm_path'];
+                                //$siteurl="http://water-mark.in/control_panel";
+                                //print_r($key);
+                                if ($i == 0) {
+                                    // first
+                                    echo '<div class="item active">';
+                                    echo '<img src="' . $siteurl . "/" . $imagepath . '" alt="">';
+                                    echo '</div>';
                                 }
-                            } else {
-                                $pro_name = $_REQUEST['name'];
-                                //print_r($pro_name);
-                                $ans = $hobj->select("*", "wm_product", "wm_catid_product='$cat' AND wm_product='$pro_name'");
-                                //print_r($ans);
-                                $i = 0;
-                                $len = count($ans);
-                                foreach ($ans as $imagepath) {
-                                    $imagepath = $imagepath['wm_path'];
-                                    //$siteurl="http://water-mark.in/control_panel";
-                                    //print_r($key);
-                                    if ($i == 0) {
-                                        // first
-                                        echo '<div class="item active">';
-                                        echo '<img src="' . $siteurl . "/" . $imagepath . '" alt="">';
-                                        echo '</div>';
-                                    }
-                                    if ($i != 0) {
-                                        echo "<div class='item'>";
-                                        echo '<img src="' . $siteurl . "/" . $imagepath . '" alt="">';
-                                        echo "</div>";
-                                    }
-                                    $i++;
+                                if ($i != 0) {
+                                    echo "<div class='item'>";
+                                    echo '<img src="' . $siteurl . "/" . $imagepath . '" alt="">';
+                                    echo "</div>";
                                 }
+                                $i++;
+                                //print_r($siteurl."/".$imagepath);
                             }
                         } else {
-                            echo '<div class="item active">
+                            $pro_name = $_REQUEST['name'];
+                            //print_r($pro_name);
+                            $ans = $hobj->select("*", "wm_product", "wm_catid_product='$cat' AND wm_product='$pro_name'");
+                            //print_r($ans);
+                            $i = 0;
+                            $len = count($ans);
+                            foreach ($ans as $imagepath) {
+                                $imagepath = $imagepath['wm_path'];
+                                //$siteurl="http://water-mark.in/control_panel";
+                                //print_r($key);
+                                if ($i == 0) {
+                                    // first
+                                    echo '<div class="item active">';
+                                    echo '<img src="' . $siteurl . "/" . $imagepath . '" alt="">';
+                                    echo '</div>';
+                                }
+                                if ($i != 0) {
+                                    echo "<div class='item'>";
+                                    echo '<img src="' . $siteurl . "/" . $imagepath . '" alt="">';
+                                    echo "</div>";
+                                }
+                                $i++;
+                            }
+                        }
+                    } else {
+                        echo '<div class="item active">
 					<img src="images/Acetech-2009/Vis-a-Vis/1.png" alt="">
 					
 					</div>';
-                        }
-                        ?>
+                    }
+                    ?>
 
 
-                    </div>
-                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
-                    <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
                 </div>
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
             </div>
+        </div>
     </div>
 </div>
 </div>
